7433e911fa6639351fc2cf9df0427d29d61c6bfd
Mohit Kumar <mohit37.my@gmail.com>
2024-01-31 23:29:07 +0530

    Added Basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-
-import styles from './App.module.css'
+import Display from "./components/Display";
+import styles from "./App.module.css";
+import ButtonsContainer from "./components/ButtonsContainer";
 
 function App() {
-
+ 
   return (
     <div className={styles.calculator}>
-      <input type="text" className={styles.display} />
-      <div className={styles.buttonsContainer}>
-        <button className={styles.button}>C</button>
-        <button className={styles.button}>1</button>
-        <button className={styles.button}>2</button>
-        <button className={styles.button}>3</button>
-      </div>
+      <Display></Display>
+     <ButtonsContainer></ButtonsContainer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
