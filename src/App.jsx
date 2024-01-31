5ca0455df4815d8cdafab957f171144a2a29812e
Mohit Kumar <mohit37.my@gmail.com>
2024-01-31 22:48:38 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <div className={styles.calculator}>
+      <input type="text" className={styles.display} />
+      <div className={styles.buttonsContainer}>
+        <button className={styles.button}>C</button>
+        <button className={styles.button}>1</button>
+        <button className={styles.button}>2</button>
+        <button className={styles.button}>3</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
